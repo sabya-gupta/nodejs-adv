240fc775819b53c0689598150aa03235d63eedfd
sabya-gupta <sabya.gupta@yahoo.com>
2020-12-19 01:32:19 +0530

    PM2 + benchmark

--- cluster-express/app.js
+++ cluster-express/app.js
@@ -33,3 +33,33 @@ if(cluster.isMaster){
 
     app.listen(3000)
 }
+/**
+Concurrency Level:      200
+Time taken for tests:   5.747 seconds
+Complete requests:      10000
+Failed requests:        0
+Total transferred:      2110000 bytes
+HTML transferred:       120000 bytes
+Requests per second:    1740.15 [#/sec] (mean)
+Time per request:       114.933 [ms] (mean)
+Time per request:       0.575 [ms] (mean, across all concurrent requests)
+Transfer rate:          358.57 [Kbytes/sec] received
+
+Connection Times (ms)
+              min  mean[+/-sd] median   max
+Connect:        0    0   0.5      0       1
+Processing:    14  113  10.7    114     153
+Waiting:        9   91  20.6     94     153
+Total:         15  113  10.7    114     154
+
+Percentage of the requests served within a certain time (ms)
+  50%    114
+  66%    117
+  75%    118
+  80%    119
+  90%    121
+  95%    123
+  98%    135
+  99%    144
+ 100%    154 (longest request)
+ */
---
+++ pm2-cluster-express/server.js
@@ -0,0 +1,53 @@
+//pm2 start server.js -i 0
+const express = require('express')
+const app = express()
+
+const testDoTimedPong=()=>{
+    const start = Date.now()
+    while((Date.now() - start) <5000){
+    }
+    return
+}
+
+app.get('/ping', (req, res)=>{
+    testDoTimedPong()
+    res.send('pong!')
+})
+
+app.get('/no-wait', (req, res)=>{
+    // testDoTimedPong()
+    res.send('Was I fast?!')
+})
+
+app.listen(3000)
+
+/**
+WITH i 4
+--------
+Complete requests:      10000
+Failed requests:        0
+Total transferred:      2110000 bytes
+HTML transferred:       120000 bytes
+Requests per second:    1668.34 [#/sec] (mean)
+Time per request:       119.879 [ms] (mean)
+Time per request:       0.599 [ms] (mean, across all concurrent requests)
+Transfer rate:          343.77 [Kbytes/sec] received
+
+Connection Times (ms)
+              min  mean[+/-sd] median   max
+Connect:        0    0   0.5      0       4
+Processing:    14  118   9.3    120     130
+Waiting:       11   82  26.1     85     128
+Total:         14  118   9.4    120     130
+
+Percentage of the requests served within a certain time (ms)
+  50%    120
+  66%    122
+  75%    123
+  80%    123
+  90%    124
+  95%    125
+  98%    126
+  99%    127
+ 100%    130 (longest request) 
+*/
